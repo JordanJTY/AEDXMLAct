30a46fa559291180387d586b5558f27e99210477
SergioManuelJob <sergiomanuelsuarezsuarez@alumno.ieselrincon.es>
2023-01-10 20:31:13 +0000

    View done

--- Application/src/main/java/View.java
+++ Application/src/main/java/View.java
@@ -1,6 +1,10 @@
 import javax.swing.*;
 
 public class View extends JFrame{
+    private JButton createXMLFileButton;
+    private JButton modifyXMLFileButton;
+    private JButton deleteXMLFileButton;
+
     public View(){
         super("Videoclub");
     }
